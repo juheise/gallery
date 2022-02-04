0b2c42b29cd9a191d5ca68b0b8f20701c12350ee
Julian Heise <jam.heise@gmail.com>
2022-02-04 11:19:07 +0100

    redirect to pictures from root

--- gallery/app.py
+++ gallery/app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template, send_file
+import flask
 
 from gallery.views.browse import fetch_thumbnails
 from gallery.views.details import load_picture
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return flask.redirect("/pictures")
 
 
 @app.route("/pictures")
 def browse():
-    return render_template("browse.html", sections=fetch_thumbnails())
+    return flask.render_template("browse.html", sections=fetch_thumbnails())
 
 
 @app.route("/pictures/<uuid>")
 def picture(uuid):
     pic = load_picture(uuid)
-    return send_file(pic["abspath"], mimetype="image/jpeg")
+    return flask.send_file(pic["abspath"], mimetype="image/jpeg")
 
 
 @app.route("/pictures/<uuid>/thumbnail")
 def thumbnail(uuid):
     pic = load_picture(uuid)
-    return send_file(pic["thumbnail"], mimetype="image/jpeg")
+    return flask.send_file(pic["thumbnail"], mimetype="image/jpeg")
